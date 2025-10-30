145a02be1aac71fa33c31df471deb669a4b9bb5f
Hotwire <hotwire2990@gmail.com>
2025-10-29 19:15:36 -0700

    Robot.java cleansed.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -50,8 +50,6 @@ import frc.robot.subsystems.*;
 import frc.robot.Constants.Tracks;
 import frc.robot.LimelightHelpers.PoseEstimate;
 
-// https://prod.liveshare.vsengsaas.visualstudio.com/join?A272BB5848034C8CE6F916D34B585613FF9F
-
 public class Robot extends LoggedRobot {
 
     private Command m_autonomousCommand;
@@ -61,70 +59,41 @@ public class Robot extends LoggedRobot {
     Field2d robotPose = new Field2d();
     Field2d nearestPoseField = new Field2d();
 
-    double slider_value = 0;
-    
-
-    private final RobotContainer m_robotContainer;
+    private final RobotContainer robotContainer;
 
     public Robot() {
-        m_robotContainer = new RobotContainer();
+        // Create robot container and upload field models.
+        robotContainer = new RobotContainer();
         SmartDashboard.putData("Limelight Pose", llestamation);
         SmartDashboard.putData("Robot Pose", robotPose);
         SmartDashboard.putData("Navigate Target Pose", nearestPoseField);
 
+        // Create limelight boolean (detecting)
         for (String limelight : Constants.LIMELIGHT_NAMES) {
             SmartDashboard.putBoolean(limelight + " detecting", false);
         }
 
-        Logger.recordMetadata("Hotwire Project", "2026"); // Set a metadata value
+        // Start logging process.
+        Logger.recordMetadata("Hotwire Project", "2026");
 
         if (isReal()) {
-            Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
-            Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+            Logger.addDataReceiver(new WPILOGWriter());
+            Logger.addDataReceiver(new NT4Publisher());
         } else {
-            setUseTiming(false); // Run as fast as possible
-            String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from
-            // AdvantageScope (or prompt the user)
-            Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+            setUseTiming(false);
+            String logPath = LogFileUtil.findReplayLog();
+            Logger.setReplaySource(new WPILOGReader(logPath));
             Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath,
-            "_sim"))); // Save outputs to a new log
+            "_sim")));
         }
 
         Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-
-        Logger.recordOutput("Intake Target State", m_robotContainer.intake.targetState);
-
-        Logger.start(); // Start logging! No more data receivers, replay sources, or
-        // metadata values may be added.
+        Logger.start();
     }
-
-    // Non-functional; Implement later
-    Orchestra m_orchestra = new Orchestra(
-        Filesystem.getDeployDirectory() + "/orchestra/mac.chrp"
-    );
-
-    double armTargetValue;
-    double wristTargetValue;
 
     @Override
     public void robotInit() {
       PathfindingCommand.warmupCommand().schedule();
-
-      armTargetValue = m_robotContainer.arm.armTarget.magnitude();
-      wristTargetValue = m_robotContainer.arm.wristTarget.magnitude();
-
-      SmartDashboard.putNumber("Wrist Target", wristTargetValue);
-      SmartDashboard.putNumber("Arm Target", armTargetValue);
-
-      // var status = m_orchestra.loadMusic("track.chrp");
-
-      // m_orchestra.addInstrument(m_robotContainer.arm.baseMotor());
-      // m_orchestra.addInstrument(m_robotContainer.arm.wristMotor());
-      // m_orchestra.addInstrument(m_robotContainer.drivetrain.getModule(0).getDriveMotor());
-      // m_orchestra.addInstrument(m_robotContainer.drivetrain.getModule(1).getDriveMotor());
-      // m_orchestra.addInstrument(m_robotContainer.drivetrain.getModule(2).getDriveMotor());
-      // m_orchestra.addInstrument(m_robotContainer.drivetrain.getModule(3).getDriveMotor());
-      // m_orchestra.play(); // TODO Make orchestra function.
     }
 
     
@@ -133,47 +102,14 @@ public class Robot extends LoggedRobot {
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
 
-        Angle value = m_robotContainer.arm.arm_encoder.getPosition().getValue();
-        SmartDashboard.putString("Arm Position", value.toString());
+        SmartDashboard.putNumber("Nearest ID", robotContainer.drivetrain.nearestId);
 
-        // Temperature temp = m_robotContainer.arm.getBaseMotor().getDeviceTemp().getValue();
-        // SmartDashboard.putNumber("Arm base Motor Temperature", (temp.in(Fahrenheit)));
+        // for (Intake.Range range : Intake.Range.values()) {
+        //   Boolean measurement = robotContainer.intake.getMeasurement(range);
+        //   SmartDashboard.putBoolean(range.toString() + " CANrange", measurement);
+        // }
 
-        Intake.TargetState target = m_robotContainer.intake.targetState;
-        Intake.SystemState current = m_robotContainer.intake.currentState;
-
-        SmartDashboard.putString("Intake TargetState", target.toString());
-        SmartDashboard.putString("Intake SystemState", current.toString());
-
-        SmartDashboard.putString("Arm TargetState", (m_robotContainer.arm.targetState).toString());
-        SmartDashboard.putString("Arm SystemState", (m_robotContainer.arm.currentState).toString());
-
-        SmartDashboard.putString("Drivetrain TargetState", (m_robotContainer.drivetrain.targetState).toString());
-        SmartDashboard.putString("Drivetrain SystemState", (m_robotContainer.drivetrain.currentState).toString());
-
-        SmartDashboard.putString("Superstructure Current State", m_robotContainer.superstructure.systemState.toString());
-        SmartDashboard.putString("Superstructure Target State", m_robotContainer.superstructure.targetState.toString());
-        
-        SmartDashboard.putNumber("Nearest ID", m_robotContainer.drivetrain.nearestId);
-
-        for (Intake.Range range : Intake.Range.values()) {
-          Boolean measurement = m_robotContainer.intake.getMeasurement(range);
-          SmartDashboard.putBoolean(range.toString() + " CANrange", measurement);
-        }
-
-        SmartDashboard.putNumber("PyRobot X", robotPose.getRobotPose().getX());
-        SmartDashboard.putNumber("PyRobot Y", robotPose.getRobotPose().getY());
-        SmartDashboard.putBoolean("PyRobot Enabled", DriverStation.isEnabled());
-// 
-        SmartDashboard.putBoolean("Is Route Complete", m_robotContainer.superstructure.routeComplete);
-
-        // wristTargetValue = SmartDashboard.getNumber("Wrist Target", 0);
-        // armTargetValue   = SmartDashboard.getNumber("Arm Target", 0);
-
-        // m_robotContainer.arm.armTarget = Rotations.of(armTargetValue);
-        // m_robotContainer.arm.wristTarget = Rotations.of(wristTargetValue);
-
-        robotPose.setRobotPose(m_robotContainer.drivetrain.getState().Pose);
+        robotPose.setRobotPose(robotContainer.drivetrain.getState().Pose);
 
         SmartDashboard.putString("Alliance", DriverStation.getAlliance().toString());
         
@@ -185,31 +121,14 @@ public class Robot extends LoggedRobot {
             double omegaRPS;
             PoseEstimate limelightMeasurement;
 
-
-            // How we should structure our sequences next season.
-            //? Button A
-            // m_robotContainer.drivetrain.navigateCommand
-            //   .alongWith(new ArmToPose(
-            //     Constants.ArmPositions.TAKE_ALGAE_L3, 
-            //     Constants.WristPositions.TAKE_ALGAE_L3, 
-            //     m_robotContainer.arm)
-            //   .andThen(new ArmToPose(
-            //     Constants.ArmPositions.TAKE_ALGAE_L3, 
-            //     Constants.WristPositions.REMOVE_ALGAE_L3,
-            //     m_robotContainer.arm)
-            //   ));
-
-            
-
             /*
              * `limelight-one` is back.
              * `limelight-two` is front.
              */
-            
             boolean detectedFlag = false;
             for (String limelight : Constants.LIMELIGHT_NAMES) {
 
-              driveState = m_robotContainer.drivetrain.getState();
+              driveState = robotContainer.drivetrain.getState();
               headingDeg = driveState.Pose.getRotation().getDegrees();
               omegaRPS = Units.radiansToRotations(driveState.Speeds.omegaRadiansPerSecond);
               /**
@@ -229,7 +148,7 @@ public class Robot extends LoggedRobot {
                   measurements.add(limelightMeasurement);
                   SmartDashboard.putBoolean(limelight + " detecting", true);
                   detectedFlag = true;
-                  m_robotContainer.drivetrain.addVisionMeasurement(limelightMeasurement.pose,
+                  robotContainer.drivetrain.addVisionMeasurement(limelightMeasurement.pose,
                     limelightMeasurement.timestampSeconds);
                   llestamation.setRobotPose(limelightMeasurement.pose);
               } else {
@@ -248,10 +167,8 @@ public class Robot extends LoggedRobot {
             }
       }
 
-      Translation2d end = new Translation2d(0, 0);
-
-      if (m_robotContainer.drivetrain.nearestPose != null) {
-        nearestPoseField.setRobotPose(m_robotContainer.drivetrain.nearestPose);
+      if (robotContainer.drivetrain.nearestPose != null) {
+        nearestPoseField.setRobotPose(robotContainer.drivetrain.nearestPose);
       }
     }
 
@@ -269,8 +186,8 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void autonomousInit() {
-      m_robotContainer.superstructure.targetState = Superstructure.TargetState.AUTONOMOUS;
-      m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+      robotContainer.superstructure.targetState = Superstructure.TargetState.AUTONOMOUS;
+      m_autonomousCommand = robotContainer.getAutonomousCommand();
 
       if (m_autonomousCommand != null) {
         m_autonomousCommand.schedule();
@@ -287,7 +204,7 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void teleopInit() {
-      m_robotContainer.superstructure.targetState = Superstructure.TargetState.DEFAULT;
+      robotContainer.superstructure.targetState = Superstructure.TargetState.DEFAULT;
       if (m_autonomousCommand != null) {
         m_autonomousCommand.cancel();
       }
@@ -295,13 +212,7 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void teleopPeriodic() {
-      // m_robotContainer.superstructure.targetState = Superstructure.TargetState.TESTING;
-      SmartDashboard.putString("Drivetrain State", 
-        m_robotContainer.drivetrain.currentState.toString()
-      );
-      SmartDashboard.putString("Superstructure State", 
-        m_robotContainer.superstructure.systemState.toString()
-      );
+      
     }
 
     @Override
